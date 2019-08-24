9673158c733a7e4e02d02bedad71f6a7ff10529c
Sucipto <chip@pringstudio.com>
2019-08-24 09:48:10 +0700

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,9 @@
 # Remove Greeting
 set fish_greeting
 
+# Local Bin
+set -gx PATH $PATH ~/.local/bin
+
 # Android SDK
 set -gx ANDROID_HOME /opt/android/SDK
 set -gx PATH $PATH $ANDROID_HOME/tools $ANDROID_HOME/platform-tools
