8f4cdaca76bf35ba6fd9426a8d962fbb26ff6b8c
Sucipto <sucipto@pm.me>
2020-06-25 10:48:40 +0700

    Add install command

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Install Vim Plug
+echo "Install Vim Plugin Manager"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Install Plugin
+echo "Install Vim Plugin"
+vim -c 'PlugInstall|q|q'
+
+
---
+++ pull.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Copy Nvim config
+cp -R ~/.config/nvim .config/
+
+# Copy Tmux config
+cp ~/.tmux.conf .
